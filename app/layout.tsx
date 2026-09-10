529ca11d2a5fb9de2d3df7e6bd94a56b5f1a93c0
akshay parab <57339768+TechiAkki963@users.noreply.github.com>
2026-09-10 23:34:20 +0530

    Load responsive workspace shell styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import "./workspace-polish.css";
 import "./master-activity.css";
 import "./knowledge.css";
 import "./mobile-workspaces.css";
+import "./workspace-shell-v2.css";
 import "./legal.css";
 
 export const metadata: Metadata = {
