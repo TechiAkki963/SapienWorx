283d4062864e28083ed84603c6e518e36a3206fe
akshay parab <57339768+TechiAkki963@users.noreply.github.com>
2026-09-10 23:35:51 +0530

    Load candidate interview styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import "./master-activity.css";
 import "./knowledge.css";
 import "./mobile-workspaces.css";
 import "./workspace-shell-v2.css";
+import "./candidate-interviews.css";
 import "./legal.css";
 
 export const metadata: Metadata = {
