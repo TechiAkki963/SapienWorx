def49939760ac80ebe89e66caa673df463e558f0
akshay parab <57339768+TechiAkki963@users.noreply.github.com>
2026-09-12 14:39:32 +0530

    feat(recruiter): route dashboard to needs-attention workbench

--- app/recruiter/page.tsx
+++ app/recruiter/page.tsx
@@ -1,6 +1,8 @@
-import { RecruiterDashboard } from "../../components/recruiter";
+import { RecruiterDashboardV2 } from "../../components/recruiter-dashboard-v2";
 import { getRecruiterDashboardSnapshot } from "../../lib/backend";
 
 export const dynamic = "force-dynamic";
 
-export default async function RecruiterPage() { return <RecruiterDashboard initialData={await getRecruiterDashboardSnapshot()} />; }
+export default async function RecruiterPage() {
+  return <RecruiterDashboardV2 initialData={await getRecruiterDashboardSnapshot()} />;
+}
