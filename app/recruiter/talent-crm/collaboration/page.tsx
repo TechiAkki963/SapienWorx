c349489bb59ddbd1f18a80a23bdc8066f030630e
akshay parab <57339768+TechiAkki963@users.noreply.github.com>
2026-09-13 04:42:33 +0530

    feat(routes): add recruiter collaboration workspace

---
+++ app/recruiter/talent-crm/collaboration/page.tsx
@@ -0,0 +1,5 @@
+import { RecruiterCollaboration } from "../../../../components/recruitment-evolution";
+
+export default function RecruiterCollaborationPage() {
+  return <RecruiterCollaboration />;
+}
