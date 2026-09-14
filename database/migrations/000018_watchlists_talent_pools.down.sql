f3d11f75a704ff7ef6e0c23906ec76cd8b0c3b80
akshay parab <57339768+TechiAkki963@users.noreply.github.com>
2026-09-15 03:49:21 +0530

    Watchlists step 2: rollback publish alert trigger

--- database/migrations/000018_watchlists_talent_pools.down.sql
+++ database/migrations/000018_watchlists_talent_pools.down.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
+DROP TRIGGER IF EXISTS trg_jobs_notify_company_watchers ON jobs;
+DROP FUNCTION IF EXISTS notify_company_watchers_on_job_publish();
 DROP INDEX IF EXISTS ux_candidate_notifications_company_watch_job;
 DROP TABLE IF EXISTS talent_pool_memberships;
 DROP TABLE IF EXISTS company_watchers;
