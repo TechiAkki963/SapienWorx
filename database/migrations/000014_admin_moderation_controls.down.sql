24241d96f2bde7f242a6daa442da365e95354a8b
akshay parab <57339768+TechiAkki963@users.noreply.github.com>
2026-09-14 05:58:33 +0530

    Complete admin moderation rollback

--- database/migrations/000014_admin_moderation_controls.down.sql
+++ database/migrations/000014_admin_moderation_controls.down.sql
@@ -4,6 +4,8 @@ DROP TRIGGER IF EXISTS trg_recruiter_profiles_create_company_verification ON rec
 DROP FUNCTION IF EXISTS create_company_verification_for_recruiter();
 DROP TRIGGER IF EXISTS trg_users_clear_force_password_reset ON users;
 DROP FUNCTION IF EXISTS clear_force_password_reset_on_password_change();
+DROP TRIGGER IF EXISTS trg_users_invalidate_password_on_forced_reset ON users;
+DROP FUNCTION IF EXISTS invalidate_password_on_forced_reset();
 DROP INDEX IF EXISTS ix_users_force_password_reset;
 DROP INDEX IF EXISTS ix_users_admin_lookup;
 ALTER TABLE users DROP COLUMN IF EXISTS force_password_reset;
