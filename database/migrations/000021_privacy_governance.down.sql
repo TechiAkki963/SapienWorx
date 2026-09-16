f9ba06b22f47cab4a95469ce6239c8bd458bb68a
akshay parab <57339768+TechiAkki963@users.noreply.github.com>
2026-09-17 02:08:03 +0530

    Add privacy governance rollback

---
+++ database/migrations/000021_privacy_governance.down.sql
@@ -0,0 +1,7 @@
+BEGIN;
+DROP TABLE IF EXISTS privacy_audit_events;
+DROP TABLE IF EXISTS privacy_retention_policies;
+DROP TABLE IF EXISTS privacy_processing_activities;
+DROP TABLE IF EXISTS privacy_subprocessors;
+DROP TABLE IF EXISTS privacy_incidents;
+COMMIT;
